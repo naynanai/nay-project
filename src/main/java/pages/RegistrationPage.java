983ae855fdf21d4a2727ed33c983b9a97a0b9e37
nayiri.meschian <nme@ciklum.com>
2017-12-27 17:58:43 +0100

    Test Refactored :)

--- src/main/java/pages/RegistrationPage.java
+++ src/main/java/pages/RegistrationPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * Created by nme on 27/12/17.
@@ -21,40 +23,80 @@ public class RegistrationPage {
     }
 
     public void goToPage(String urlToNavigate) {
-        driver.get(urlToNavigate);
+        driver.get( urlToNavigate );
     }
 
-    public void closePage() {
+    public void waitSomeSeconds (int time) {
+        driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
+    }
+
+    public void closeAndQuitPage() {
         driver.close();
         driver.quit();
+    }
+
+    public void RegisterIsSuccesfull () {
+        WebElement confirmationMessage = driver.findElement(By.className( "piereg_message" ));
+        confirmationMessage.isDisplayed();
     }
 
     public String getTitle() {
         return driver.getTitle();
     }
 
-    public void clickRegistrationLink () {
+    public void clickRegistrationLink() {
         WebElement registrationLink = driver.findElement( By.linkText( "Registration" ) );
         registrationLink.click();
     }
 
-    public void fillFirstNameField (String username) {
-        WebElement firstName = driver.findElement(By.id("name_3_firstname"));
-        firstName.sendKeys(username);
+    public void clickSubmittButton() {
+        WebElement submitButton = driver.findElement( By.name( "pie_submit" ) );
+        submitButton.click();
     }
 
-    public void fillLastNameField (String surname) {
-        WebElement lastName = driver.findElement(By.id("name_3_lastname"));
-        lastName.sendKeys(surname);
+    public void fillFirstNameField(String username) {
+        WebElement firstName = driver.findElement( By.id( "name_3_firstname" ) );
+        firstName.sendKeys( username );
     }
 
-    public void selectCheckbox (String checkboxValue) {
-        WebElement statusCheckBox = driver.findElement(By.cssSelector(checkboxValue));
+    public void fillLastNameField(String surname) {
+        WebElement lastName = driver.findElement( By.id( "name_3_lastname" ) );
+        lastName.sendKeys( surname );
+    }
+
+    public void selectCheckbox(String checkboxValue) {
+        WebElement statusCheckBox = driver.findElement( By.cssSelector( checkboxValue ) );
         statusCheckBox.click();
     }
 
-    public void selectDropdown (String dropDown, String value) {
-        Select countrySelector = new Select(driver.findElement(By.id(dropDown)));
-        countrySelector.selectByVisibleText(value);
+    public void selectDropdown(String dropDown, String value) {
+        Select countrySelector = new Select( driver.findElement( By.id( dropDown ) ) );
+        countrySelector.selectByVisibleText( value );
     }
+
+    public void fillPhoneNumber(String userPhone) {
+        WebElement phoneNumber = driver.findElement( By.name( "phone_9" ) );
+        phoneNumber.sendKeys( userPhone );
+    }
+
+    public void fillUserName(String nickname) {
+        WebElement username = driver.findElement( By.name( "username" ) );
+        username.sendKeys( nickname );
+    }
+
+    public void fillEmail(String userMail) {
+        WebElement email = driver.findElement( By.name( "e_mail" ) );
+        email.sendKeys( userMail );
+    }
+
+    public void fillPassword(String userPassword) {
+        WebElement password = driver.findElement( By.name( "password" ) );
+        password.sendKeys( userPassword );
+    }
+
+    public void confirmPassword(String repeatPassword) {
+        WebElement confirmPassword = driver.findElement( By.id( "confirm_password_password_2" ) );
+        confirmPassword.sendKeys( repeatPassword );
+    }
+
 }
--- src/test/java/tests/NayTest02.java
+++ src/test/java/tests/NayTest02.java
@@ -2,16 +2,7 @@ package tests;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import pages.RegistrationPage;
-
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -35,13 +26,16 @@ public class NayTest02  {
         Thread.sleep( 2000 );
         assertTrue( "Page is not Registration form", registrationPage.getTitle().equals( "Registration | Demoqa" ) );
 
-        registrationPage.closePage();
+        registrationPage.closeAndQuitPage();
     }
 
+    // * Created randomizer for username
+    // * Create randomizer for email
     @Test
-    public void testSimpleRegistrationSuccessfulPRO() throws InterruptedException {
+    public void testSimpleRegistrationSuccessful() throws InterruptedException {
 
         registrationPage.goToPage("http://demoqa.com/registration/");
+
         registrationPage.fillFirstNameField( "Nay" );
         registrationPage.fillLastNameField("Nanai ");
         registrationPage.selectCheckbox("input[value='divorced']" );
@@ -50,69 +44,16 @@ public class NayTest02  {
         registrationPage.selectDropdown( "mm_date_8", "12" );
         registrationPage.selectDropdown( "dd_date_8", "10" );
         registrationPage.selectDropdown( "yy_date_8", "1983" );
-        Thread.sleep( 2000 );
-        registrationPage.closePage();
-    }
+        registrationPage.fillPhoneNumber( "0034123456789" );
+        registrationPage.fillUserName( "Nayiro de la Puebla" );
+        registrationPage.fillEmail( "testo+testi@testi.com" );
+        registrationPage.fillPassword( "12345678" );
+        registrationPage.confirmPassword( "12345678" );
+        registrationPage.clickSubmittButton();
 
-    @Test
-    public void testSimpleRegistrationSuccessful() throws InterruptedException {
-
-        System.setProperty( "webdriver.chrome.driver", "/Users/nme/work/chromedriver" );
-        WebDriver driver = new ChromeDriver();
-        driver.get( "http://demoqa.com/registration/" );
-
-        WebElement firstName = driver.findElement(By.id("name_3_firstname"));
-        firstName.sendKeys("Nay");
-
-        WebElement lastName = driver.findElement( By.id( "name_3_lastname" ) );
-        lastName.sendKeys("Nanay");
-
-        WebElement statusCheckBox = driver.findElement(By.cssSelector("input[value='divorced']"));
-        statusCheckBox.click();
-
-        WebElement hobbyCheckBox = driver.findElement(By.cssSelector("input[value='reading']"));
-        hobbyCheckBox.click();
-
-        Select countrySelector = new Select(driver.findElement(By.id("dropdown_7")));
-        countrySelector.selectByVisibleText("Argentina");
-
-        Select monthSelector = new Select(driver.findElement(By.id("mm_date_8")));
-        monthSelector.selectByVisibleText("12");
-
-        Select daySelector = new Select(driver.findElement(By.id("dd_date_8")));
-        daySelector.selectByVisibleText("10");
-
-        Select yearSelector = new Select(driver.findElement(By.id("yy_date_8")));
-        yearSelector.selectByVisibleText("1983");
-
-        WebElement phoneNumber = driver.findElement( By.name( "phone_9" ) );
-        phoneNumber.sendKeys("0034123456789");
-
-        WebElement username = driver.findElement( By.name( "username" ) );
-        username.sendKeys("Natiira de la Pueblas");
-
-        WebElement email = driver.findElement( By.name( "e_mail" ) );
-        email.sendKeys("naydelagfgfebles@pueblas.com");
-
-        WebElement password = driver.findElement( By.name( "password" ) );
-        password.sendKeys("12345678");
-
-        WebElement confirmPassword = driver.findElement( By.id( "confirm_password_password_2" ) );
-        confirmPassword.sendKeys("12345678");
-
-        WebElement submitButton = driver.findElement(By.name( "pie_submit" ));
-        submitButton.click();
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        WebElement confirmationMessage = driver.findElement(By.className( "piereg_message" ));
-        confirmationMessage.isDisplayed();
-
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        driver.close();
-        driver.quit();
-
+        registrationPage.waitSomeSeconds(5);
+        registrationPage.RegisterIsSuccesfull();
+        registrationPage.closeAndQuitPage();
     }
 
 }
